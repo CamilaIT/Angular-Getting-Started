51f3accc62c87b93ec589179bb6b54e71e74ffa8
c.rodrigues.camillo <c.rodrigues.camillo@WINAPR5RIAYG79N>
2022-09-09 18:23:56 -0300

    Página de produtos

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -4,7 +4,8 @@ import { Component } from "@angular/core";
   selector: 'pm-root',
   template: `<div>
     <h1>{{ pageTitle }}</h1>
-    <div>First App</div>
+    <pm-products></pm-products>
+    
   </div>`
 })
 
